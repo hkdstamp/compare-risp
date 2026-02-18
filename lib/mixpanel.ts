ebdde161ea3f28791da6ae2637d3b2f22cbab240
Kodo <kodo.haruhiko@alphuas.cloud>
2026-02-18 23:06:04 +0900

    remove token

--- lib/mixpanel.ts
+++ lib/mixpanel.ts
@@ -5,7 +5,7 @@ export function initMixpanel() {
     return
   }
 
-  const token = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN ?? '090356d730eb3f9cfbe474d039f13716';
+  const token = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
   if (!token) {
     console.warn('NEXT_PUBLIC_MIXPANEL_TOKEN is not set')
     return
